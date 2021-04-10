ef4456cc741ed7d3036a99a4fa6e1ee053defe65
hiresfilip <moltres1888@seznam.cz>
2021-04-10 21:58:20 +0200

    03 - Modely type a instrument

--- music/models.py
+++ music/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
-# Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Type name",
+                            help_text='Enter a type of musical instrument')
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Instrument(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    history = models.TextField(blank=True, null=True, verbose_name="History")
+    type = models.ManyToManyField(Type, help_text='Select a type for this instrument')
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        """Součástí textové reprezentace filmu bude jeho název, rok uvedení a hodnocení"""
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('instrument-detail', args=[str(self.id)])
