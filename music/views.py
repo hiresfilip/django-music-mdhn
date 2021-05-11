612d3bf792129167098bd89d60bf67ef09c1d23e
hiresfilip <moltres1888@seznam.cz>
2021-05-11 21:45:26 +0200

    06 - Parzovani stranek, type_list, new_instruments, mathfilters

--- music/views.py
+++ music/views.py
@@ -26,3 +26,21 @@ class InstrumentListView(ListView):
     context_object_name = 'instrument_list'
     template_name = 'instrument/list.html'
     paginate_by = 2
+
+
+    def get_queryset(self):
+        if 'typ_name' in self.kwargs:
+            return Instrument.objects.filter(genres__name=self.kwargs['typ_name']).all() # Get 5 books containing the title war
+        else:
+            return Instrument.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        if 'typ_name' in self.kwargs:
+            context['view_title'] = f"Typ: {self.kwargs['typ_name']}"
+            context['view_head'] = f"Typ nástroje: {self.kwargs['typ_name']}"
+        else:
+            context['view_title'] = 'Typy'
+            context['view_head'] = 'Přehled typů nastoje'
+        return context
